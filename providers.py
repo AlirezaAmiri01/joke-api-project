064521db3a7d04aeee6f75c70cebde4daa5d8f15
Alireza <a.amiri.pariyan.@gmail.com>
2026-06-30 23:00:25 +0330

    Add implementations for joke providers

--- providers.py
+++ providers.py
@@ -1,16 +1,37 @@
+import requests
+
+
 class JokeAPIProvider(Joke):
 
     def get_random_joke(self):
-        pass
+        url = "https://v2.jokeapi.dev/joke/Any"
+
+        response = requests.get(url)
+
+        data = response.json()
+
+        if data["type"] == "single":
+            return data["joke"]
+        if data["type"] == "twopart":
+            return data["setup"] + "\n" + data["delivery"]
 
 
 class OfficialJokeProvider(Joke):
 
     def get_random_joke(self):
-        pass
+        url = "https://official-joke-api.appspot.com/random_joke"
+        response = requests.get(url)
+        data = response.json()
+        return data["setup"] + "\n" + data["punchline"]
 
 
 class DadJokeProvider(Joke):
 
     def get_random_joke(self):
-        pass
+        url = "https://icanhazdadjoke.com/"
+        headers = {
+            "Accept": "application/json"
+        }
+        response = requests.get(url, headers=headers)
+        data = response.json()
+        return data["joke"]
