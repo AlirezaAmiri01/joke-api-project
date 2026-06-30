95b81eb6a659b5cd6e430c619e3fb7eec1539fc1
Alireza <a.amiri.pariyan.@gmail.com>
2026-07-01 00:55:05 +0330

    Imlement interactive joke menu

--- main.py
+++ main.py
@@ -7,17 +7,65 @@ from providers import (
 )
 
 
+def get_random_joke():
+    providers_list = [
+        JokeAPIProvider(),
+        OfficialJokeProvider(),
+        DadJokeProvider()
+    ]
+
+    provider = random.choice(providers_list)
+    return provider.get_random_joke()
+
+
+def get_jokeapi_joke():
+    provider = JokeAPIProvider()
+    return provider.get_random_joke()
+
+
+def get_official_joke():
+    provider = OfficialJokeProvider()
+    return provider.get_random_joke()
+
+
+def get_dad_joke():
+    provider = DadJokeProvider()
+    return provider.get_random_joke()
+
+
 def menu():
-    print("===== JOKE GENRATOR =====")
+    print("===== JOKE GENERATOR =====")
     print("1.Random Joke")
     print("2.JokeAPI")
     print("3.Official Joke")
     print("4.Dad Joke")
-    print("5.Exit")
+    print("0.Exit")
 
 
 def main():
-    pass
+    while True:
+        menu()
+
+        choice = int(input("Enter your choice(number): "))
+
+        if choice == 1:
+            print(get_random_joke())
+
+        elif choice == 2:
+            print(get_jokeapi_joke())
+
+        elif choice == 3:
+            print(get_official_joke())
+
+        elif choice == 4:
+            print(get_dad_joke())
+
+        elif choice == 0:
+            print("bye")
+            break
+
+        else:
+            print("pleas enter a number from 0 to 4")
 
 
 if __name__ == "__main__":
